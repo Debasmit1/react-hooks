73bed4ec9f0b455d5055af0038df277a44fc0f29
DebasmitBiswal <debasmitbiswal@outlook.com>
2023-08-22 12:09:00 +0530

    Pushing Effect file

---
+++ src/components/Effect.js
@@ -0,0 +1,24 @@
+import React , {useState,useEffect} from "react";
+
+const Effect = () => {
+    const [windowWidth,setWindowWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth)
+    }
+
+    useEffect(()=>{
+        window.addEventListener('resize',handleResize)
+
+        return ()=> (
+            window.removeEventListener('resize',handleResize)
+        )
+
+    },[])
+
+    return(
+        <div>{windowWidth}</div>
+    )
+}
+
+export default Effect;
